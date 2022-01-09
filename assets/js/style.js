1e2e481b9d99fc0340490bea59035071d803b381
Sandeep <guptasandy876@gmail.com>
2022-01-09 19:07:56 +0530

    update user

--- assets/js/style.js
+++ assets/js/style.js
@@ -19,5 +19,29 @@
       alert('Data Insert Successfully');
     })
 
+    $('#update_user').submit(function(event){
+      event.preventDefault();
+
+      var data_array= $(this).serializeArray();
+      var data = {};
+
+      $.map(data_array,function(n,i){
+        data[n['name']]=n['value']
+      })
+
+      console.log(data.id);
+
+      var request = {
+        "url":'api/users/${data.id}',
+        "method" : 'PUT',
+        "data":data
+      }
+
+      $.ajax(request).done(function(res){
+        alert('Data Updated Successfully');
+      })
+
+      
+    })
 
     
--- server/services/render.js
+++ server/services/render.js
@@ -2,6 +2,7 @@ const axios =require('axios');
 
 
 exports.homeRoutes =(req,res)=>{
+
     axios.get('http://localhost:3000/api/users')
     .then(data=>{
         console.log(data);
@@ -18,5 +19,14 @@ exports.add_user =(req,res)=>{
 }
 
 exports.update_user =(req,res)=>{
-    res.render('update_user');
+    axios.get('http://localhost:3000/api/users',{params:{id:req.query.id}})
+    .then(userdata=>{
+        console.log(userdata);
+        res.render('update_user',{users:userdata.data});
+    })
+    .catch(err => {
+        res.send(err);
+    })
+
+    // res.render('update_user');
 }
